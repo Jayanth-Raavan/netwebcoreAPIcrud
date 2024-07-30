3c3ad243597e58eb16c3125820bc29da8144db32
Jayanth Bochu <jayanth.bochu@healthonus.io>
2024-07-31 00:46:58 +0530

    Initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPI.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
